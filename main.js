7605dcac6c86e0ccfff62d1ad17760a16313f929
srk9110 <65576400+srk9110@users.noreply.github.com>
2020-12-21 22:08:11 +0900

    js project animation

--- main.js
+++ main.js
@@ -59,6 +59,36 @@ arrowBtn.addEventListener('click',()=>{
 });
 
 
+//프로젝트 버튼 필터링
+const workBtnContainer =document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click',(event)=>{
+    const filter=event.target.dataset.filter || event.target.parentNode.dataset.filter;
+    if(filter==null){
+        return;
+    }
+
+    projectContainer.classList.add('anim-out');
+
+    setTimeout(()=>{
+
+        projects.forEach((project)=>{
+            if(filter==='*' || filter ===project.dataset.type){
+                project.classList.remove('invisible');
+            } else {
+                project.classList.add('invisible');
+            } 
+        }); 
+
+        projectContainer.classList.remove('anim-out');
+    },250);
+    
+    
+});
+
+
 //이동함수
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
