2deed3a9be6be8d79538f2125d7ccb692cc53bde
srk9110 <65576400+srk9110@users.noreply.github.com>
2020-12-21 19:08:55 +0900

    navbar: scrolling

--- main.js
+++ main.js
@@ -11,3 +11,24 @@ document.addEventListener('scroll', ()=>{
         navbar.classList.remove('navbar--dark');
     }
 });
+
+
+//네비게이션 버튼 스크롤링
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(event)=>{
+    const target = event.target;
+    const link = target.dataset.link;
+    
+    if(link==null){
+        return;
+    }
+
+    console.log(link);
+    const scrollTo = document.querySelector(link);
+    console.log(scrollTo);
+    scrollTo.scrollIntoView({behavior:'smooth', block:'start'});
+});
+
+
+
